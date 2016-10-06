1f889281c8cbbd55ed4e3f9f3e337405564e2f59
Mahan <eng.mahan.hazrati@gmail.com>
2016-10-06 18:34:56 +0700

    bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Reservation</title>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css">
+
         <script src="/js/react.js"></script>
     </head>
     <body>
