5036943afde6acda10352239ad310e3a03674f3a
Mahan <eng.mahan.hazrati@gmail.com>
2016-10-12 00:30:40 +0700

    fix the price and avalibility component

--- resources/assets/js/components/price-and-availibility.jsx
+++ resources/assets/js/components/price-and-availibility.jsx
@@ -1,85 +1,75 @@
 import React, { Component } from 'react';
+import TimeTableEntity from './time-table-entity.jsx'
 
 export default class PriceAndAvailibility extends Component {
-    render() {
-      return (
-        <form className="form-inline form-price-and-availibility">
-          <div className="left-panel">
-            <div className="row-price-availibility">
-              <span>Price and Availability</span>
-            </div>
-            <div className="row-single-room">
-              <span>Single Room</span>
-            </div>
-            <div className="row-room-available">
-              <span>Room Available</span>
-            </div>
-            <div className="row-price">
-              <span>Price</span>
-            </div>
-            <div className="row-double-room">
-              <span>Double Room</span>
-            </div>
-            <div className="row-room-available">
-              <span>Room Available</span>
-            </div>
-            <div className="row-price">
-              <span>Price</span>
-            </div>
+  render() {
+    return (
+      <form className="form-inline form-price-and-availibility">
+        <div className="left-panel">
+          <div className="row-price-availibility">
+            <span>Price and Availability</span>
           </div>
-          <div className="right-panel">
-            <div className="top-date-picker">
+          <div className="row-single-room">
+            <span>Single Room</span>
+          </div>
+          <div className="row-room-available">
+            <span>Room Available</span>
+          </div>
+          <div className="row-price">
+            <span>Price</span>
+          </div>
+          <div className="row-double-room">
+            <span>Double Room</span>
+          </div>
+          <div className="row-room-available">
+            <span>Room Available</span>
+          </div>
+          <div className="row-price">
+            <span>Price</span>
+          </div>
+        </div>
+        <div className="right-panel">
+          <div className="top-date-picker">
+            <span className="date-picker-month-navigator
+              glyphicon glyphicon-triangle-left">
+            </span>
+            <div className="date-picker-month-navigator-dropdown">
+              <select>
+                <option value="1">January</option>
+                <option value="2">February</option>
+                <option value="3">March</option>
+                <option value="4">April</option>
+                <option value="5">May</option>
+                <option value="6">June</option>
+                <option value="7">July</option>
+                <option value="8">August</option>
+                <option value="9">September</option>
+                <option value="10">October</option>
+                <option value="11">November</option>
+                <option value="12">December</option>
+              </select>
               <span className="date-picker-month-navigator
-                glyphicon glyphicon-triangle-left">
-              </span>
-              <div className="date-picker-month-navigator-dropdown">
-                <select>
-                  <option value="1">January</option>
-                  <option value="2">February</option>
-                  <option value="3">March</option>
-                  <option value="4">April</option>
-                  <option value="5">May</option>
-                  <option value="6">June</option>
-                  <option value="7">July</option>
-                  <option value="8">August</option>
-                  <option value="9">September</option>
-                  <option value="10">October</option>
-                  <option value="11">November</option>
-                  <option value="12">December</option>
-                </select>
-                <span className="date-picker-month-navigator
-                  glyphicon glyphicon-triangle-bottom"></span>
-              </div>
-              <span>2016</span>
-              <span className="date-picker-month-navigator
-                glyphicon glyphicon-triangle-right"></span>
+                glyphicon glyphicon-triangle-bottom"></span>
             </div>
+            <span>2016</span>
+            <span className="date-picker-month-navigator
+              glyphicon glyphicon-triangle-right"></span>
+          </div>
+          <div className="time-table-wrapper">
             <div className="time-table">
-              <div className="row-price-availibility">
-                <span>Price and Availability</span>
-              </div>
-              <div className="row-single-room">
-                <span>Single Room</span>
-              </div>
-              <div className="row-room-available">
-                <span>Room Available</span>
-              </div>
-              <div className="row-price">
-                <span>Price</span>
-              </div>
-              <div className="row-double-room">
-                <span>Double Room</span>
-              </div>
-              <div className="row-room-available">
-                <span>Room Available</span>
-              </div>
-              <div className="row-price">
-                <span>Price</span>
-              </div>
+              <TimeTableEntity
+                dayName="Sunday"
+                dayNumber={0}
+                singleRoomAvailable={0}
+                singleRoomPrice={0}
+                doubleRoomAvailable={0}
+                doubleRoomPrice={0}
+                />
             </div>
           </div>
-          <div className="clearfix"></div>
-        </form>
-      );
-    }
+        </div>
+        <div className="clearfix"></div>
+      </form>
+    );
+  }
 }
---
+++ resources/assets/js/components/time-table-entity.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+
+export default class TimeTableEntity extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = props;
+
+    console.log(this.state);
+  }
+
+  capitalise(input) {
+    if (!input) {
+      return;
+    }
+
+    return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+  }
+
+  isWeekend(day) {
+    if (day ) {
+      day = day.toLowerCase();
+    }
+
+    return (day === 'saturday' || day === 'sunday')
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState(props);
+  }
+
+  render() {
+    return (
+      <div className="time-table-entity">
+        <div className="row-price-availibility">
+          <div className={
+              "day-name" +
+              (this.isWeekend(this.state.dayName) ? " weekend" : '')
+            }>
+            {this.capitalise(this.state.dayName)}
+          </div>
+          <div className="day-number">
+            <a href="javascript:void(0);">
+              {this.state.dayNumber}
+            </a>
+          </div>
+        </div>
+        <div className="row-single-room"></div>
+        <div className="row-room-available">
+          <a href="javascript:void(0);">
+            {this.state.singleRoomAvailable}
+          </a>
+        </div>
+        <div className="row-price">
+          <a href="javascript:void(0);">
+            {this.state.singleRoomPrice}
+          </a>
+        </div>
+        <div className="row-double-room"></div>
+        <div className="row-room-available">
+          <a href="javascript:void(0);">
+            {this.state.doubleRoomAvailable}
+          </a>
+        </div>
+        <div className="row-price">
+          <a href="javascript:void(0);">
+            {this.state.doubleRoomPrice}
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
